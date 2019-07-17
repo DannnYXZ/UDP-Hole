cafa28b3736e792e0860a0117387e436b9179f44
DannnYX <icandos@mail.ru>
2019-07-17 23:39:43 +0300

    adding connected host once

--- connect_to_udp_hole.py
+++ connect_to_udp_hole.py
@@ -1,21 +1,21 @@
-import sys
 import socket
 import time
 import threading
 import pprint
-import struct
 import raw_udp_ip
 
 AUTH_TOKEN = b'\x14\x94\xef\x03W=d\xf8\xf1\xa9\x18\x97\x05\x85cJyyd\rk\x82X\x13L\x00\xf5\x1c*\xf7LJ\t\xee\xca~`\x8ci\xe7%\x85A\xea\xd7\xe1\x88iM@\xc3\xb4\x07\xa1b\xe1\xf1\x9b\xddjQ6*\xaa'
 AUTH_TOKEN = b'KEKEKEK'
 PING_ATTEMPTS = 3
-SUSTAIN_DELTA_SEC = 15
+SUSTAIN_DELTA_SEC = 10
 
 connected_hosts = []
 probable_hosts = []
 
 pp = pprint.PrettyPrinter()
 sniff_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+
+
 def sniff_for_host():
     while True:
         ip_with_udp = sniff_sock.recv(65535)
@@ -24,7 +24,8 @@ def sniff_for_host():
         src_addr = (ip_info['src_ip'], udp_info['src_port'])
         dst_addr = (ip_info['dst_ip'], udp_info['dst_port'])
         if udp_info['data'] == AUTH_TOKEN:
-            connected_hosts.append((src_addr, dst_addr))
+            if not (src_addr, dst_addr) in connected_hosts:
+                connected_hosts.append((src_addr, dst_addr))
         else:
             found = False
             for record in probable_hosts:
@@ -42,6 +43,7 @@ def sniff_for_host():
 
 def unwanted_ip(ip):
     return ip in ['0.0.0.0', '127.0.0.1']
+
 
 def ping_probable_hosts():
     while True:
@@ -64,9 +66,11 @@ def ping_probable_hosts():
 
 def sustain_connected_hosts():
     while True:
-        for to_addr, from_addr in probable_hosts.copy():
-            raw_udp_ip.send_udp('ಠᴗಠ', from_addr, to_addr)
-    time.sleep(SUSTAIN_DELTA_SEC)
+        for to_addr, from_addr in connected_hosts.copy():
+            print(to_addr, from_addr)
+            raw_udp_ip.send_udp('^___^', from_addr, to_addr)
+            print('SUSTAIN:', to_addr)
+        time.sleep(SUSTAIN_DELTA_SEC)
 
 
 # raw_udp_ip.send_udp('KEKEKEK', ('192.168.43.91', 443), ('192.168.43.91', 52132)) 
@@ -74,4 +78,4 @@ def sustain_connected_hosts():
 
 threading.Thread(target=sniff_for_host).start()
 threading.Thread(target=ping_probable_hosts).start()
-# threading.Thread(target=sustain_connected_hosts).start()
+threading.Thread(target=sustain_connected_hosts).start()
